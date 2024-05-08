b1a0b4be4a9e55e47dce20d6cd9052bf923f8cb8
Wai Lin Aung <wla.6wla9@gmail.com>
2024-05-08 15:12:43 +0630

    leap year quiz update

--- wailinaung/leapyear/leapyear.go
+++ wailinaung/leapyear/leapyear.go
@@ -6,7 +6,7 @@ func main() {
 	var year int
 	fmt.Println("enter a year...")
 	fmt.Scanf("%d", &year)
-	if year%4 == 0 {
+	if year%4 == 0 && year%100 != 0 || year%400 == 0 {
 		fmt.Printf("%d is leap year", year)
 	} else {
 		fmt.Printf("%d isn't leap year", year)
