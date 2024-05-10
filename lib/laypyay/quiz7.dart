fd74867cfc05323a2a603eb29044932e4f3d90c4
laypyay28 <laypyay17931@gmail.com>
2024-05-10 14:28:35 -0700

    quiz 7- pre

---
+++ lib/laypyay/quiz7.dart
@@ -0,0 +1,9 @@
+import "dart:io";
+
+void main(){
+  print("enter first coin");
+ int coin1 = int.parse(stdin.readLineSync()!);
+
+   print("enter second coin");
+ int coin2 = int.parse(stdin.readLineSync()!);
+}
